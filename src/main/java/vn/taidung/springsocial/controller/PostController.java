0d274c8accd76f7d0db4bb02f1795cc040f28320
Taidung <taidung3010@gmail.com>
2025-10-22 23:58:51 +0700

    updatePostDto + update/delete post

--- src/main/java/vn/taidung/springsocial/controller/PostController.java
+++ src/main/java/vn/taidung/springsocial/controller/PostController.java
@@ -2,7 +2,9 @@ package vn.taidung.springsocial.controller;
 
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.taidung.springsocial.model.Post;
 import vn.taidung.springsocial.model.request.CreatePostRequest;
+import vn.taidung.springsocial.model.request.UpdatePostRequest;
 import vn.taidung.springsocial.service.PostService;
 
 @RestController
@@ -24,11 +27,21 @@ public class PostController {
 
     @PostMapping("/posts")
     public Post createPost(@RequestBody CreatePostRequest postRequest) {
-        return postService.createPostHandler(postRequest);
+        return this.postService.createPostHandler(postRequest);
     }
 
     @GetMapping("/posts/{id}")
     public Optional<Post> getPost(@PathVariable Long id) {
-        return postService.getPostHandler(id);
+        return this.postService.getPostHandler(id);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deletePost(@PathVariable Long id) {
+        this.postService.deletePostHandler(id);
+    }
+
+    @PatchMapping("/posts/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody UpdatePostRequest postRequest) {
+        return this.postService.updatePostHandler(id, postRequest);
     }
 }
--- src/main/java/vn/taidung/springsocial/service/PostService.java
+++ src/main/java/vn/taidung/springsocial/service/PostService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import vn.taidung.springsocial.model.Post;
 import vn.taidung.springsocial.model.request.CreatePostRequest;
+import vn.taidung.springsocial.model.request.UpdatePostRequest;
 import vn.taidung.springsocial.repository.PostRepository;
 
 @Service
@@ -19,13 +21,35 @@ public class PostService {
         this.modelMapper = modelMapper;
     }
 
+    @Transactional
     public Post createPostHandler(CreatePostRequest postRequest) {
-        Post post = modelMapper.map(postRequest, Post.class);
+        Post post = this.modelMapper.map(postRequest, Post.class);
         return this.postRepository.save(post);
     }
 
     public Optional<Post> getPostHandler(Long id) {
-        return postRepository.findById(id);
+        return this.postRepository.findById(id);
+    }
+
+    public void deletePostHandler(Long id) {
+        this.postRepository.deleteById(id);
+    }
+
+    public Post updatePostHandler(Long id, UpdatePostRequest updatePostRequest) {
+        Optional<Post> optionalPost = this.postRepository.findById(id);
+        if (optionalPost.isPresent()) {
+            Post post = this.modelMapper.map(optionalPost.get(), Post.class);
+
+            if (updatePostRequest.getTitle() != null) {
+                post.setTitle(updatePostRequest.getTitle());
+            }
+
+            if (updatePostRequest.getContent() != null) {
+                post.setContent(updatePostRequest.getContent());
+            }
+            return this.postRepository.save(post);
+        }
+        return null;
     }
 
 }
